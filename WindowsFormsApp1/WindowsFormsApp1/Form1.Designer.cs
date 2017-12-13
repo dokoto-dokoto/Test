fb0c76789bb475a9d9e30572e6d70967acbc9270
LAPTOP-C0EALSGI\ishikawashota <ss2805koneko@icloud.com>
2017-12-13 19:43:24 +0900

    伍長ボタン配置

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(294, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(421, 307);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "伍長";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 735);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
